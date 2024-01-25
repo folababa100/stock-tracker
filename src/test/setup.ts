3b51d5749d6481c561688d484f56bbfe2a92c004
Adekunbi Adefolarin <dekunbifola100@gmail.com>
2024-01-25 08:48:45 +0100

    chore: cleanup

--- src/test/setup.ts
+++ src/test/setup.ts
@@ -1,11 +1,5 @@
-// import { afterEach } from 'vitest';
-// import { cleanup } from '@testing-library/react';
-//
-// afterEach(() => {
-//   cleanup();
-// });
 import { vi } from 'vitest';
-import '@testing-library/jest-dom';
+import '@testing-library/jest-dom'; // MatchMedia Mock
 
 // MatchMedia Mock
 const global: Window = window;
